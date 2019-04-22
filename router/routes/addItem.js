eccc08fbfeed381cd8f8103311b6b4160d64885f
Trishala Kaushik <trishala08@Trishalas-Air.fios-router.home>
2019-04-22 08:52:51 -0400

    addItem

--- router/routes/addItem.js
+++ router/routes/addItem.js
@@ -12,9 +12,14 @@ router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 router.post('/', function (req, res) {
   //var postData  = req.body;
 console.log("Reached add item");
- db.query('INSERT INTO belongings (belonging_name, belongings_desc) VALUES(?, ?)', [req.body.belonging_name, req.body.belongings_desc], function (error, results, fields) {
+ db.query('INSERT INTO belongings (belongings_name, belongings_desc, belongings_category) VALUES(?, ?, ?)', [req.body.belongings_name, req.body.belongings_desc, req.body.belongings_category], function (error, results, fields) {
        if (error) throw error;
        else{
+         db.query('SELECT belongings_id from belongings where belongings_name=?', [req.body.belongings_name], function (error, results, fields) {
+           db.query('INSERT INTO beneficiary_belongings (belongings_id, will_id) VALUES(?, ?)', [results[0].belongings_id, req.query.willid], function (error, results, fields) {
+         });
+       });
+
          var details = 'Added Item ' + req.body.belonging_name ;
          db.query('INSERT INTO log (will_id, log_details) VALUES (?,?)',[req.query.willid, details], function (error, results, fields) {
          });
