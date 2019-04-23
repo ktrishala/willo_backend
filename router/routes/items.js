922cd161d7ee758eee553d14e42ef8ab8e31e00f
Trishala Kaushik <trishala08@Trishalas-Air.fios-router.home>
2019-04-23 19:07:16 -0400

    desciption

--- router/routes/items.js
+++ router/routes/items.js
@@ -13,7 +13,7 @@ router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 router.get('/', function (req, res) {
   console.log("Reached here item;");
    console.log(req.query.willid);
-   db.query('SELECT belongings_id, belongings_name, belongings_category from belongings where belongings_id in (select belongings_id from beneficiary_belongings where will_id =?)', [req.query.willid], function (error, results, fields) {
+   db.query('SELECT belongings_id, belongings_name, belongings_category, belongings_desc from belongings where belongings_id in (select belongings_id from beneficiary_belongings where will_id =?)', [req.query.willid], function (error, results, fields) {
   	  if (error) throw error;
   	  res.end(JSON.stringify(results));
    });
