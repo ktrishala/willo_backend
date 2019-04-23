02caa6acf523d8fe0087919208652d4aaa629c59
Trishala Kaushik <trishala08@Trishalas-Air.fios-router.home>
2019-04-23 11:54:28 -0400

    belongings_name

--- router/routes/getBeneficiaryAssetMapping.js
+++ router/routes/getBeneficiaryAssetMapping.js
@@ -10,16 +10,9 @@ router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 }));
 
 
-// router.get('/', function (req, res) {
-// db.query('SELECT b.name as name, c.belonging_name as belonging_name from beneficiary_belongings a inner join user b on a.user_id = b.user_id inner join belongings c on a.belongings_id = c.belongings_id where a.user_id=? and a.belongings_id=? and a.will_id=?;' ,req.query.benid, req.query.itemid, req.query.willid, function (error, results, fields) {
-//         if (error) throw error;
-//         res.send(JSON.stringify(results));
-//       });
-// });
-
 router.get('/', function (req, res) {
-  console.log("Reached ben asset mapping");
-db.query('SELECT b.name as name, c.belonging_name as belonging_name from beneficiary_belongings a inner join user b on a.user_id = b.user_id inner join belongings c on a.belongings_id = c.belongings_id where a.will_id=?;' ,req.query.willid, function (error, results, fields) {
+  console.log("Reached ben asset mapping 2");
+db.query('SELECT b.name as name, c.belongings_name as belongings_name from beneficiary_belongings a inner join user b on a.user_id = b.user_id inner join belongings c on a.belongings_id = c.belongings_id where a.will_id=?;' ,req.query.willid, function (error, results, fields) {
         if (error) throw error;
         res.send(JSON.stringify(results));
       });
