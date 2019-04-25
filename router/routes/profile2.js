6f2e506c2f8a70ce66246c91674d5785a7889ac4
Trishala Kaushik <trishala08@Trishalas-Air.fios-router.home>
2019-04-25 00:00:33 -0400

    profile2

--- router/routes/profile2.js
+++ router/routes/profile2.js
@@ -11,40 +11,69 @@ router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 
 
-// router.get('/check', function (req, res) {
-//    db.query('select * from user', function (error, results, fields) {
+
+
+//var query = 'UPDATE user SET marital_status = ?, children = ?  WHERE  email = ?';
+router.post('/', function (req, res) {
+  //var marital_status = req.body.marital_status;
+  for(var key in req.nameofchild.hasOwnProperty(key)){
+    console.log(req.nameofchild[key].name)
+  }
+  // var children = req.body.children;
+  // var email  = req.body.email;
+  //  db.query(query, [marital_status, children, email], function (error, results, fields) {
+	//   if (error) throw error;
+	//   else if(children.localeCompare("Y")==0){
+  //     console.log("Entered here", children);
+  //     db.query('SELECT user_id from user where email=?', [email], function (error, results, fields) {
+  //       var user_id = results[0].user_id;
+  //       db.query('SELECT will_id from parties where user_id=? and party_type="owner"', [user_id], function (error, results1, fields) {
+  //         var will_id=results1[0].will_id;
+  //         db.query('UPDATE will SET children =? where will_id=?', [children, will_id], function (error, results, fields) {
+  //         });
+  //         db.query('INSERT INTO children (will_id, children_name, children_dob) VALUES(?, ?, ?)', [will_id, req.body.children_name, req.body.children_dob], function (error, results, fields) {
+  //           res.send({
+  //             "code":400,
+  //             "result":true,
+  //             "msg": "success"
+  //           })
+  //         });
+  //       });
+  //     });
+  //   }
+	// });
+});
+
+
+
+
+// var query = 'UPDATE user SET marital_status = ?, children = ?  WHERE  email = ?';
+// router.post('/', function (req, res) {
+//   var marital_status = req.body.marital_status;
+//   var children = req.body.children;
+//   var email  = req.body.email;
+//    db.query(query, [marital_status, children, email], function (error, results, fields) {
 // 	  if (error) throw error;
-// 	  res.end(JSON.stringify(results));
+// 	  else if(children.localeCompare("Y")==0){
+//       console.log("Entered here", children);
+//       db.query('SELECT user_id from user where email=?', [email], function (error, results, fields) {
+//         var user_id = results[0].user_id;
+//         db.query('SELECT will_id from parties where user_id=? and party_type="owner"', [user_id], function (error, results1, fields) {
+//           var will_id=results1[0].will_id;
+//           db.query('UPDATE will SET children =? where will_id=?', [children, will_id], function (error, results, fields) {
+//           });
+//           db.query('INSERT INTO children (will_id, children_name, children_dob) VALUES(?, ?, ?)', [will_id, req.body.children_name, req.body.children_dob], function (error, results, fields) {
+//             res.send({
+//               "code":400,
+//               "result":true,
+//               "msg": "success"
+//             })
+//           });
+//         });
+//       });
+//     }
 // 	});
 // });
-
-var query = 'UPDATE user SET marital_status = ?, children = ?  WHERE  email = ?';
-router.post('/', function (req, res) {
-  var marital_status = req.body.marital_status;
-  var children = req.body.children;
-  var email  = req.body.email;
-   db.query(query, [marital_status, children, email], function (error, results, fields) {
-	  if (error) throw error;
-	  else if(children.localeCompare("Y")==0){
-      console.log("Entered here", children);
-      db.query('SELECT user_id from user where email=?', [email], function (error, results, fields) {
-        var user_id = results[0].user_id;
-        db.query('SELECT will_id from parties where user_id=? and party_type="owner"', [user_id], function (error, results1, fields) {
-          var will_id=results1[0].will_id;
-          db.query('UPDATE will SET children =? where will_id=?', [children, will_id], function (error, results, fields) {
-          });
-          db.query('INSERT INTO children (will_id, children_name, children_dob) VALUES(?, ?, ?)', [will_id, req.body.children_name, req.body.children_dob], function (error, results, fields) {
-            res.send({
-              "code":400,
-              "result":true,
-              "msg": "success"
-            })
-          });
-        });
-      });
-    }
-	});
-});
 
 // router.get('/user/:id', function (req, res) {
 //    console.log(req);
