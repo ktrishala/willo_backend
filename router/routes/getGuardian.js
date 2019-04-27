d67593331eedf30735cd123ef91fa866a832390c
Trishala Kaushik <trishala08@Trishalas-Air.fios-router.home>
2019-04-27 12:27:40 -0400

    getguardian

--- router/routes/getGuardian.js
+++ router/routes/getGuardian.js
@@ -14,16 +14,9 @@ router.get('/', function (req, res) {
    console.log(req.query.willid);
    db.query('select a.user_id as user_id, a.name as name, a.email as email, a.contact as contact,b.party_type as party_type from user a inner join parties b on a.user_id=b.user_id and will_id=? and party_type in("primary guardian","secondary guardian")', [req.query.willid], function (error, results, fields) {
 	  if (error) throw error;
-    if(results.length===0){
-      res.send({
-        "code":400,
-        "result":true,
-        "msg":"No witness added yet"
-      });
-    }
-    else{
+
       res.end(JSON.stringify(results));
-    }
+
 
 	});
 });
