e1cd93a4e7d355fc65986175a7a3405bad0a324a
Trishala Kaushik <trishala08@Trishalas-Air.fios-router.home>
2019-04-27 19:20:11 -0400

    executor

--- router/routes/getExecutors.js
+++ router/routes/getExecutors.js
@@ -8,31 +8,18 @@ router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }));
 
-router.get('/', function (req, res) {
-  console.log("Reached here executors details;");
-   console.log(req.query.willid);
-   db.query('SELECT * FROM parties WHERE will_id = ? and party_type in ("primary executor", "secondary executor")', [req.query.willid], function (error, results, fields) {
-     if(results.length===0){
-       res.send({
-         "code":400,
-         "result":false,
-         "msg":"No Executor added yet"
-       });
-     }
-     else if(results.length===1){
-       db.query('SELECT user_id, name, email, dob, relationship, contact from user where user_id=? ', [results[0].user_id], function (error, results, fields) {
-             if (error) throw error;
-              res.send(JSON.stringify(results));
-           });
-     }
-     else if(results.length===2){
-       db.query('SELECT user_id, name, email, dob, relationship, contact from user where user_id in(?,?)', [results[0].user_id, results[1].user_id], function (error, results, fields) {
-             if (error) throw error;
-             res.send(JSON.stringify(results));
-           });
 
-     }
-  });
+router.get('/', function (req, res) {
+  console.log("Reached here list executors;");
+   console.log(req.query.willid);
+   db.query('select a.user_id as user_id, a.name as name, a.email as email, a.contact as contact,b.party_type as party_type from user a inner join parties b on a.user_id=b.user_id and will_id=? and party_type in("primary executor","secondary executor")', [req.query.willid], function (error, results, fields) {
+	  if (error) throw error;
+
+      res.end(JSON.stringify(results));
+
+
+	});
 });
+
 
 module.exports = router;
