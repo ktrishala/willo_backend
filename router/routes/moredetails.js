9109069b786aa8c3d3c69ac822190147085dfac2
Trishala Kaushik <trishala08@Trishalas-MacBook-Air.local>
2019-05-04 18:11:35 -0400

    profilechanges

--- router/routes/moredetails.js
+++ router/routes/moredetails.js
@@ -18,21 +18,22 @@ router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 // 	});
 // });
 
-var query = 'UPDATE user SET contact=?, dob = ?, gender=?  WHERE  user_id = ?';
+var query = 'UPDATE user SET street_address = ?, county = ? , city = ?, state = ?, zip=?  WHERE  user_id = ?';
 router.post('/', function (req, res) {
   var will_id = req.query.willid;
-  var contact = req.body.contact;
-  var dob = req.body.dob;
-  var gender = req.body.gender;
+  var street_address = req.body.street_address;
+  var county = req.body.county;
+  var city = req.body.city;
+  var state = req.body.state;
   //var own_property = req.body.own_property;
+  var zip =req.body.zip;
   console.log("Reached here for updating profile 1");
-  console.log(req.body.gender);
 
   db.query('SELECT user_id from parties where will_id=? and party_type="owner"', [will_id], function (error, results, fields) {
    if (error) throw error;
    if(results.length>0){
      var user_id = results[0].user_id;
-     db.query(query, [contact, dob, gender, user_id], function (error, results, fields) {
+     db.query(query, [street_address, county, city, state, zip, user_id], function (error, results, fields) {
   	  if (error) throw error;
       res.send({
         "code":400,
