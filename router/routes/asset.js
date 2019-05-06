d7c6f8e90864a11b0eb85b1bcd9153e0f535dd40
Trishala Kaushik <trishala08@Trishalas-Air.fios-router.home>
2019-05-06 00:31:25 -0400

    admin portal

--- router/routes/asset.js
+++ router/routes/asset.js
@@ -17,7 +17,7 @@ router.get('/', function (req, res) {
      var query3 = 'SELECT log_details, DATE(log_ts) as last_updated_dt from log where will_id=? order by log_ts desc limit 1';
      db.query(query3, [req.query.will_id], function (error, results3, fields) {
        console.log(JSON.stringify(results3));
-       results.push(JSON.stringify(results3));
+       results.push(results3);
       res.send(JSON.stringify(results));
     });
 
