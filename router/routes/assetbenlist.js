10596cce9e74bbb1665b706f8f4d09d8eded8a22
Trishala Kaushik <trishala08@Trishalas-MacBook-Air.local>
2019-05-06 16:56:14 -0400

    update

--- router/routes/assetbenlist.js
+++ router/routes/assetbenlist.js
@@ -10,7 +10,7 @@ router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 router.get('/', function (req, res) {
    console.log("The will id is:"+req.query.will_id);
-   db.query('SELECT c.belongings_id as belongings_id, c.belongings_name as belongings_name, GROUP_CONCAT(DISTINCT b.name order by b.name) as name from beneficiary_belongings a inner join user b on a.user_id = b.user_id inner join belongings c on a.belongings_id = c.belongings_id where a.will_id=? GROUP BY belongings_id, belongings_name',[req.query.will_id], function (error, results, fields) {
+   db.query('SELECT c.belongings_id as belongings_id, c.belongings_name as belongings_name, GROUP_CONCAT(DISTINCT b.name order by b.name) as name from beneficiary_belongings a inner join user b on a.user_id = b.user_id inner join belongings c on a.belongings_id = c.belongings_id where a.will_id=? GROUP BY belongings_id, belongings_name',[req.query.willid], function (error, results, fields) {
     if (error) throw error;
     console.log(JSON.stringify(results));
     res.send(JSON.stringify(results));
