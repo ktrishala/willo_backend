80e077d688436ce4b393e8c26353c05b1e67ccf3
Trishala Kaushik <trishala08@Trishalas-Air.fios-router.home>
2019-05-07 01:36:09 -0400

    update1234

--- router/routes/mapBeneficiarytoItem.js
+++ router/routes/mapBeneficiarytoItem.js
@@ -12,55 +12,59 @@ router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 router.post('/', function (req, res) {
   //var postData  = req.body;
 console.log("Reached map item to beneficiary");
-console.log(req.body);
-for(key in req.body.postData){
+console.log(req.body[0]);
+for(key in req.body){
   console.log(req.query.willid);
-  console.log(req.body.postData[key].belongings_id);
-  console.log(req.body.postData[key].pct_allocation);
-  console.log(req.body.postData[key].user_id);
-  db.query('SELECT SUM(pct_allocation) as sum_pct_allocation from beneficiary_belongings where will_id=? and belongings_id=?', [req.query.willid, parseInt(req.body.postData[key].belongings_id)], function (error, results, fields) {
+  console.log(req.body[key].belongings_id);
+  console.log(req.body[key].pct_allocation);
+  console.log(req.body[key].user_id);
+  db.query('SELECT SUM(pct_allocation) as sum_pct_allocation from beneficiary_belongings where will_id=? and belongings_id=?', [req.query.willid, parseInt(req.body[key].belongings_id)], function (error, results, fields) {
       if (error) throw error;
       else if(results[0].sum_pct_allocation===100){
         console.log("Entered here 1");
-        res.send({
-          "code":400,
-          "result":false,
-          "msg": "This item is already allocated 100%"
-        });
+        // res.send({
+        //   "code":400,
+        //   "result":false,
+        //   "msg": "This item is already allocated 100%"
+        // });
       }
-      else if((parseInt(req.body.postData[key].pct_allocation)+ results[0].sum_pct_allocation)<=100 && results[0].sum_pct_allocation===0){
+      else if((parseInt(req.body[key].pct_allocation)+ results[0].sum_pct_allocation)<=100 && results[0].sum_pct_allocation===0){
         console.log("Entered here 2");
-        db.query('UPDATE beneficiary_belongings SET user_id =?, pct_allocation = ? where will_id=? and belongings_id=?', [parseInt(req.body.postData[key].user_id), parseInt(req.body.postData[key].pct_allocation), req.query.willid, (req.body.postData[key].belongings_id)], function (error, results, fields) {
-              res.send({
-                "code":200,
-                "result":true,
-                "msg": "Sucessfully added"
-              });
+        db.query('UPDATE beneficiary_belongings SET user_id =?, pct_allocation = ? where will_id=? and belongings_id=?', [parseInt(req.body[key].user_id), parseInt(req.body[key].pct_allocation), req.query.willid, (req.body[key].belongings_id)], function (error, results, fields) {
+              // res.send({
+              //   "code":200,
+              //   "result":true,
+              //   "msg": "Sucessfully added"
+              // });
         });
       }
-      else if((parseInt(req.body.postData[key].pct_allocation)+ results[0].sum_pct_allocation)<=100 && results[0].sum_pct_allocation!=0){
+      else if((parseInt(req.body[key].pct_allocation)+ results[0].sum_pct_allocation)<=100 && results[0].sum_pct_allocation!=0){
         console.log("Entered here 3");
-        db.query('INSERT INTO beneficiary_belongings (belongings_id, will_id, user_id, pct_allocation) VALUES(?, ?, ?, ?)', [parseInt(req.body.postData[key].belongings_id), req.query.willid, parseInt(req.body.postData[key].user_id), parseInt(req.body.postData[key].pct_allocation)], function (error, results, fields) {
-              res.send({
-                "code":200,
-                "result":true,
-                "msg": "Successfully Added"
-              });
+        db.query('INSERT INTO beneficiary_belongings (belongings_id, will_id, user_id, pct_allocation) VALUES(?, ?, ?, ?)', [parseInt(req.body[key].belongings_id), req.query.willid, parseInt(req.body[key].user_id), parseInt(req.body[key].pct_allocation)], function (error, results, fields) {
+              // res.send({
+              //   "code":200,
+              //   "result":true,
+              //   "msg": "Successfully Added"
+              // });
         });
       }
-      else if((parseInt(req.body.postData[key].pct_allocation)+ results[0].sum_pct_allocation)>100){
+      else if((parseInt(req.body[key].pct_allocation)+ results[0].sum_pct_allocation)>100){
         console.log("Entered here 4");
         var remaining_pct =100- results[0].sum_pct_allocation;
-              res.send({
-                "code":400,
-                "result":false,
-                "msg": "Pct Allocation exceeds 100%",
-                "remaining_pct" : remaining_pct
-              });
+              // res.send({
+              //   "code":400,
+              //   "result":false,
+              //   "msg": "Pct Allocation exceeds 100%",
+              //   "remaining_pct" : remaining_pct
+              // });
       }
   });
 }
 
-
+res.send({
+   "code":200,
+   "result":true,
+   "msg": "Successfully Added"
+ });
 });
 module.exports = router;
