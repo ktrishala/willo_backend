03ace2d2cf8c3e50a684b16bbf5bad4af2ebcccd
Trishala Kaushik <trishala08@Trishalas-MacBook-Air.local>
2019-05-08 18:11:43 -0400

    updatel

--- router/routes/testmail.js
+++ router/routes/testmail.js
@@ -15,10 +15,12 @@ router.get('/', function (req, res) {
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 const msg = {
   to: 'tkaushik64@gmail.com',
-  from: 'test@example.com',
+  from: 'hello@willo.com',
   subject: 'Sending with Twilio SendGrid is Fun',
   text: 'and easy to do anywhere, even with Node.js',
   html: '<strong>and easy to do anywhere, even with Node.js</strong>',
 };
 sgMail.send(msg);
 });
+
+module.exports = router;
