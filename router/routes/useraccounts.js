c6fd099259c6c7a9e619c123c0a6731b756d81ed
Trishala Kaushik <trishala08@Trishalas-MacBook-Air.local>
2019-05-08 17:25:09 -0400

    updatel

--- router/routes/useraccounts.js
+++ router/routes/useraccounts.js
@@ -10,11 +10,15 @@ router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 router.get('/', function (req, res) {
   console.log("Reached useraccounts in server");
-   db.query('select c.name as name, c.email as email, c.user_id as user_id, a.will_id as will_id from will a inner join parties b on a.will_id = b.will_id and b.party_type="owner" inner join user c on b.user_id=c.user_id group by name, email, user_id, will_id ', function (error, results, fields) {
+   db.query('select c.name as name, c.email as email, c.user_id as user_id, a.will_id as will_id, a.will_status as will_status from will a inner join parties b on a.will_id = b.will_id and b.party_type="owner" inner join user c on b.user_id=c.user_id group by name, email, user_id,will_id ', function (error, results, fields) {
     if (error) throw error;
     console.log(JSON.stringify(results));
     res.send(JSON.stringify(results));
   });
 });
+
+
+
+
 
 module.exports = router;
