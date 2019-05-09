0e335f6de15ee0a4813d7597523aaf8ab0c96b6b
Trishala Kaushik <tkaushik64@gmail.com>
2019-05-09 19:36:34 -0400

    mailupdate134p

--- router/routes/getamount.js
+++ router/routes/getamount.js
@@ -10,29 +10,50 @@ router.use(bodyParser.urlencoded({     // to support URL-encoded bodies
 
 router.post('/', function (req, res) {
   console.log("Reached here to get amount for user");
-   console.log(req.query.willid);
-   console.log(req.body.discount_type);
-   var discount_value= req.body.discount_value;
-   var discount_type= req.body.discount_type;
-   if(discount_type=="Whole"){
-     db.query('SELECT (annual_sub_price-discount_value) as price from subscription_model, discount where discount_type=? and discount_value=? and activity_flag="Active"', [discount_type, discount_value], function (error, results, fields) {
-  	  if (error) throw error;
-  	  res.end(JSON.stringify(results));
-  	});
-   }
-   else if (discount_type=="Percent") {
-     db.query('SELECT (annual_sub_price-(discount_value*0.01*annual_sub_price)) as price from subscription_model, discount where discount_type=? and discount_value=? and activity_flag="Active"', [discount_type, discount_value], function (error, results, fields) {
-  	  if (error) throw error;
-  	  res.end(JSON.stringify(results));
-  	});
-   }
-   else {
-     db.query('SELECT annual_sub_price as price from subscription_model', [discount_type, discount_value], function (error, results, fields) {
-  	  if (error) throw error;
-  	  res.end(JSON.stringify(results));
-  	});
-   }
-
+  console.log(req.query.willid);
+  db.query('SELCT * from discount where promo_code=? and activity_flag="Active"', [promo_code], function (error, results, fields) {
+    if(results[0].length>0){
+      var discount_value= results[0].discount_value;
+      var discount_type= results[0].discount_type;
+      if(discount_type=="Whole"){
+        db.query('SELECT (annual_sub_price-discount_value) as price from subscription_model, discount where discount_type=? and discount_value=? and activity_flag="Active"', [discount_type, discount_value], function (error, results, fields) {
+     	  if (error) throw error;
+        var price= results[0].price;
+        res.send({
+          "result":true,
+          "price":price
+        });
+     	  //res.end(JSON.stringify(results));
+     	});
+      }
+      else if (discount_type=="Percent") {
+        db.query('SELECT (annual_sub_price-(discount_value*0.01*annual_sub_price)) as price from subscription_model, discount where discount_type=? and discount_value=? and activity_flag="Active"', [discount_type, discount_value], function (error, results, fields) {
+     	  if (error) throw error;
+        var price= results[0].price;
+        res.send({
+          "result":true,
+          "price":price
+        });
+     	});
+      }
+      else {
+        db.query('SELECT annual_sub_price as price from subscription_model', [discount_type, discount_value], function (error, results, fields) {
+     	  if (error) throw error;
+        var price= results[0].price;
+        res.send({
+          "result":true,
+          "price":price
+        });
+     	});
+      }
+    }
+    else{
+      res.send({
+        "result":false,
+        "msg":"Invalid code"
+      });
+    }
+  });
 });
 
 module.exports = router;
