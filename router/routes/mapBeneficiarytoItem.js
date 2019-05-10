3ee7ee4916486a720a6b81b540b28d8e3ee70f8a
Trishala Kaushik <tkaushik64@gmail.com>
2019-05-10 00:25:34 -0400

    mailupdate134p

--- router/routes/mapBeneficiarytoItem.js
+++ router/routes/mapBeneficiarytoItem.js
@@ -38,6 +38,14 @@ for(key in req.body){
       else if((parseInt(req.body[key].pct_allocation)+ results[0].sum_pct_allocation)<=100 && results[0].sum_pct_allocation===0){
         console.log("Entered here 2");
         db.query('UPDATE beneficiary_belongings SET user_id =?, pct_allocation = ? where will_id=? and belongings_id=?', [parseInt(req.body[key].user_id), parseInt(req.body[key].pct_allocation), req.query.willid, (req.body[key].belongings_id)], function (error, results, fields) {
+          db.commit(function(err) {
+            if (err) {
+          connection.rollback(function() {
+            throw err;
+          });
+        }
+        console.log('success!');
+      });
           db.query(query, [req.query.willid, parseInt(req.body[key].belongings_id)], function (error, results123, fields) {
             console.log("Inside UPDATE");
             console.log(results123);
@@ -53,6 +61,14 @@ for(key in req.body){
         console.log("Entered here 3");
         db.query('INSERT INTO beneficiary_belongings (belongings_id, will_id, user_id, pct_allocation) VALUES(?, ?, ?, ?)', [parseInt(req.body[key].belongings_id), req.query.willid, parseInt(req.body[key].user_id), parseInt(req.body[key].pct_allocation)], function (error, results, fields) {
             console.log("Inside Insert");
+            db.commit(function(err) {
+              if (err) {
+            connection.rollback(function() {
+              throw err;
+            });
+          }
+          console.log('success!');
+        });
               // res.send({
               //   "code":200,
               //   "result":true,
